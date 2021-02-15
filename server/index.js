50c53f1313ce6e7c24b78e92c7bb8d75ccc9ae0b
kwlahr <kwlahr@gmail.com>
2021-02-15 14:24:29 -0700

    updated import syntax to const syntax in index.js. Start script now working

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import mongoose from 'mongoose';
+// import cors from 'cors';
+
+// import { express } from "express";
+// import { bodyParser } from "body-parser";
+// import { mongoose } from "mongoose";
+// import { cors } from "cors";
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,7 +20,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://kwlahr:Ky13rocks!@cluster0.vdtbz.mongodb.net/<dbname>?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT|| 3001;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
