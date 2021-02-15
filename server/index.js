bf04fa7dc9b7a81e741a6a89650dc3efd9d3daa5
kwlahr <kwlahr@gmail.com>
2021-02-15 13:08:31 -0700

    created server connection in server/index.js: not currently working

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://kwlahr:Ky13rocks!@cluster0.vdtbz.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
