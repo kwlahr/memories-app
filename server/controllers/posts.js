4a2c9c6ba9a5608576a62971406e0e7a406d8c85
kwlahr <kwlahr@gmail.com>
2021-07-13 18:14:43 -0500

    added getPosts controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
 
-module.exports = getPosts = (req, res) => {
-  res.send("THIS WORKS!");
+
+const PostMessage = require('../models/postMessage.js');
+
+
+module.exports = getPosts = async (req, res) => {
+  try {
+    
+    const postMessages = await PostMessage.find();
+
+    res.status(200).json(postMessages);
+    
+  } catch (error) {
+    res.status(404).json({ message: error.message});
+  }
+}
+
+module.exports = createPost = (req, res) => {
+  res.send("Post Creation");
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const getPosts = require('../controllers/posts.js');
+const createPost = require('../controllers/posts.js');
 
 const router = express.Router();
 
 router.get("/", getPosts );
+router.post("/", createPost );
 
 module.exports = router;
 
