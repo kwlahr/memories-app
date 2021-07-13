c5c6cbe56e652a475a6f06c75ae5bae558144f34
kwlahr <kwlahr@gmail.com>
2021-07-13 18:38:29 -0500

    set up component folder structure

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -17,6 +17,17 @@ module.exports = getPosts = async (req, res) => {
   }
 }
 
-module.exports = createPost = (req, res) => {
-  res.send("Post Creation");
+module.exports = createPost = async (req, res) => {
+   const post = req.body;
+   
+   const newPost = new PostMessage(post);
+   
+  try {
+    await newPost.save();
+
+    res.status(201).json(newPost);
+    
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 }
